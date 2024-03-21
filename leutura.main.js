9abae6b1729ccccdc7155095d6f80baf9e4237ae
Makerjunior <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>
2024-03-21 10:52:03 -0300

    updade

---
+++ leutura.main.js
@@ -0,0 +1,56 @@
+function iniciarLeitura() {
+    const botaoLer = document.getElementById('botaoLer');
+    const audioPlayer = document.getElementById('audioPlayer');
+    let utterance;
+    let isLendo = false;
+
+    botaoLer.addEventListener('click', function () {
+        // Encontrar o primeiro elemento de conteúdo após o cabeçalho
+        const primeiroElementoDeConteudo = document.querySelector('body > *:not(header)');
+        
+        // Verificar se foi encontrado algum elemento de conteúdo
+        if (primeiroElementoDeConteudo) {
+            const texto = pegarTextoElemento(primeiroElementoDeConteudo);
+            if (!isLendo) {
+                isLendo = true;
+                botaoLer.classList.add('pausado'); // Adiciona a classe 'pausado' para alterar o ícone
+                botaoLer.textContent = 'Pausar';
+                if (!utterance) {
+                    utterance = new SpeechSynthesisUtterance(texto);
+                    utterance.lang = 'pt-BR'; // Define o idioma como português do Brasil
+                    utterance.onend = function () {
+                        isLendo = false;
+                        botaoLer.textContent = 'Ouvir';
+                        botaoLer.classList.remove('pausado'); // Remove a classe 'pausado' para alterar o ícone de volta para play
+                    };
+                } else {
+                    window.speechSynthesis.resume();
+                }
+                utterance.text = texto;
+                window.speechSynthesis.speak(utterance);
+                audioPlayer.src = URL.createObjectURL(new Blob([texto], { type: 'text/plain' }));
+            } else {
+                isLendo = false;
+                botaoLer.classList.remove('pausado'); // Remove a classe 'pausado' para alterar o ícone de volta para play
+                botaoLer.textContent = 'Ouvir';
+                window.speechSynthesis.pause();
+            }
+        } else {
+            alert('Não há conteúdo para ler.');
+        }
+    });
+
+    // Função para pegar o texto de um elemento e seus filhos
+    function pegarTextoElemento(elemento) {
+        let texto = '';
+        const filhos = elemento.childNodes;
+        filhos.forEach(filho => {
+            if (filho.nodeType === Node.TEXT_NODE) {
+                texto += filho.textContent.trim() + ' ';
+            } else if (filho.nodeType === Node.ELEMENT_NODE) {
+                texto += pegarTextoElemento(filho);
+            }
+        });
+        return texto.trim();
+    }
+}
