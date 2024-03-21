b1ed4d07fa3f66d345dd15c37aa5fb1f927f8451
Makerjunior <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>
2024-03-21 09:48:20 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// minhaBiblioteca.js
+
+var minhaBiblioteca = {
+    saudacao: function(nome) {
+        return 'Olá, ' + nome + '!';
+    },
+    soma: function(a, b) {
+        return a + b;
+    }
+};
